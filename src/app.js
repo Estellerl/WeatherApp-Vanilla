addcaabb9254efe8591746b6b53b584f7172a5f8
Estellerl <Estellerl@users.noreply.github.com>
2020-08-17 12:24:51 +0100

    Added current date and time

--- src/app.js
+++ src/app.js
@@ -1,3 +1,27 @@
+function createDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minuites = date.getMinutes();
+  if (minuites < 10) {
+    minuites = `0${miniutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  return `${day} ${hours}: ${minuites}`;
+}
+
 function displayMainTemparature(response) {
   let temparature = document.querySelector("#temp");
   temparature.innerHTML = Math.round(response.data.main.temp);
@@ -9,11 +33,14 @@ function displayMainTemparature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let humidityElement = document.querySelector("#hum");
   humidityElement.innerHTML = response.data.main.humidity;
+  let dateElement = document.querySelector("#currentDate");
+  dateElement.innerHTML = createDate(response.data.dt * 1000);
 }
 
 let apiKey = "b9dcade6ea8b84ffbd9565650e525892";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bordeaux&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
-
 axios.get(apiUrl).then(displayMainTemparature);
+
+//To do:
+//Need to capitalise only one letter for description of weather
