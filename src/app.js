ed1fe33547914edae8380cc523de028cad697153
Estellerl <Estellerl@users.noreply.github.com>
2020-08-17 10:38:03 +0100

    Started JS
    Imported axios and created apis, tested and working

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
+function displayTemparature(response) {
+  console.log(response.data);
+}
 
+let city = "New York";
+let apiKey = "b9dcade6ea8b84ffbd9565650e525892";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemparature);
