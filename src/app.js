7f0208a70c3b27deffd66eab577e6df0c2f1873e
Estellerl <Estellerl@users.noreply.github.com>
2020-08-17 11:58:41 +0100

    Js- real time weather info
    Real time information for a chosen city

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-function displayTemparature(response) {
-  console.log(response.data);
+function displayMainTemparature(response) {
+  let temparature = document.querySelector("#temp");
+  temparature.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  let humidityElement = document.querySelector("#hum");
+  humidityElement.innerHTML = response.data.main.humidity;
 }
 
-let city = "New York";
 let apiKey = "b9dcade6ea8b84ffbd9565650e525892";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Bordeaux&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
 
-axios.get(apiUrl).then(displayTemparature);
+axios.get(apiUrl).then(displayMainTemparature);
